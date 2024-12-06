c322bea3d3c424c5556d7cb2460b6161ad8de2e5
Newtoone <msamota17112003@gmail.com>
2024-12-06 16:38:45 +0530

    Version 2.2

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
+import { EditorState } from "@codemirror/state";
+import { EditorView } from "@codemirror/view";
+import { basicSetup } from "codemirror";
+import { autocompletion } from "@codemirror/autocomplete";
+import { htmlLanguage } from "@codemirror/lang-html";
+import { cssLanguage } from "@codemirror/lang-css";
+import { javascriptLanguage } from "@codemirror/lang-javascript";
+import { oneDark } from "@codemirror/theme-one-dark";
 
 const Editor = ({ value, onChange, language }) => {
-  return (
-    <textarea
-      className="w-full h-full p-2 border border-gray-300 rounded-lg bg-black"
-      value={value}
-      onChange={onChange}
-      placeholder={`Write your ${language} code here...`}
-    />
-  );
+  const editorRef = useRef();
+
+  useEffect(() => {
+    const languageExtension =
+      language === "HTML"
+        ? htmlLanguage
+        : language === "CSS"
+        ? cssLanguage
+        : javascriptLanguage;
+
+    const state = EditorState.create({
+      doc: value,
+      extensions: [
+        basicSetup,
+        languageExtension,
+        autocompletion(), // Enable autocomplete
+        oneDark, // Dark theme for the editor
+        EditorView.updateListener.of((update) => {
+          if (update.docChanged) {
+            const newValue = update.state.doc.toString();
+            onChange(newValue);
+          }
+        }),
+      ],
+    });
+
+    const view = new EditorView({
+      state,
+      parent: editorRef.current,
+    });
+
+    return () => {
+      view.destroy();
+    };
+  }, [language, value, onChange]);
+
+  return <div ref={editorRef} className="w-full h-full bg-black" />;
 };
 
 export default Editor;
